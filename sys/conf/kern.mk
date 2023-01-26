7b2319ec4f35e12b00cba813fcdd0c036a337c82
Shawn Webb <shawn.webb@hardenedbsd.org>
2023-01-26 13:05:45 -0500

    HBSD: Provide a method for kernel modules to opt into -ftrivial-var-auto-init=zero
    Add a TRIVIAL_VAR_AUTO_INIT_ZERO_SAFE flag to tell the kernel build
    framework that it's safe to enable -ftrivia-var-auto-init=zero for
    kernel modules that want to opt-in.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    MFC-to:		13-STABLE
    (cherry picked from commit dd21b931eca8e5370a6d0341908316538b52de71)
    Signed-off-by: Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -235,7 +235,9 @@ CFLAGS+=	-fstack-protector
 CFLAGS+=	-mretpoline
 .endif
 
+.if !defined(TRIVIAL_VAR_AUTO_INIT_ZERO_SAFE)
 CFLAGS+=	-ftrivial-auto-var-init=uninitialized
+.endif
 
 CFLAGS+= ${CWARNFLAGS:M*} ${CWARNFLAGS.${.IMPSRC:T}}
 CFLAGS+= ${CWARNFLAGS.${COMPILER_TYPE}}
