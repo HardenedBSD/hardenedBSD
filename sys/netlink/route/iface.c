3fce4b20bdedac8fb0ed3dad6296e28a6f1d93ac
Shawn Webb <shawn.webb@hardenedbsd.org>
2023-03-07 12:11:33 -0500

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/netlink/route/iface.c
+++ sys/netlink/route/iface.c
@@ -459,12 +459,8 @@ rtnl_handle_getlink(struct nlmsghdr *hdr, struct nlpcb *nlp, struct nl_pstate *n
 					break;
 				}
 				if (match_array != NULL) {
-<<<<<<< HEAD
-					memcpy(new_array, match_array, offset * sizeof(void *));
-=======
 					memcpy(new_array, match_array,
 					    offset * sizeof(void *));
->>>>>>> freebsd/stable/13
 					free(match_array, M_TEMP);
 				}
 				match_array = new_array;
