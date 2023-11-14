2180db2ff7221f5539c0067e4f0bcce66997d39b
Shawn Webb <shawn.webb@hardenedbsd.org>
2023-11-14 14:50:39 -0700

    HBSD: Chase FreeBSD build framework changes
    FreeBSD is simplifying the LLVM toolchain knobs, so we need to adjust
    accordingly.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -517,11 +517,6 @@ MK_LLVM_COV:= no
 MK_SAFESTACK:=	no
 .endif
 
-.if ${MK_LLD_IS_LD} == "no" || ${MK_LLD_BOOTSTRAP} == "no"
-MK_CFI:=	no
-MK_RETPOLINE:=	no
-.endif
-
 .if ${MK_ASAN} == "yes"
 # In order to get sensible backtraces from ASAN we have to install
 # llvm-symbolizer as /usr/bin/addr2line instead of the elftoolchain version.
