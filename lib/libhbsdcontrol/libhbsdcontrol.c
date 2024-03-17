7e703a2c1f3e63b3be734a2ba2245c8f32f295fd
Shawn Webb <shawn.webb@hardenedbsd.org>
2024-03-17 00:20:51 +0000

    HBSD: New data structures and core API for libhbsdcontrol
    hbsdcontrol(8) uses libhbsdcontrol to handle performing the actual
    toggling of security features on a per-file basis. The underlying design
    of libhbsdcontrol and hbsdcontrol leave a bit to be desired. It's
    currently difficult to write bindings for non-C languages.
    
    This commit begins the rewrite of libhbsdcontrol. Attention is paid to
    the data structures that govern the toggling of features.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    MFC-to:		14-STABLE
    MFC-to:		13-STABLE
    (cherry picked from commit ec5780f02e088bcfe46b8c749b400c3b7aed803c)
    Signed-off-by: Shawn Webb <shawn.webb@hardenedbsd.org>

---
+++ lib/libhbsdcontrol/libhbsdcontrol.c
@@ -0,0 +1,387 @@
+/*-
+ * Copyright (c) 2024 Shawn Webb <shawn.webb@hardenedbsd.org>
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "libhbsdcontrol.h"
+
+uint64_t
+libhbsdctrl_get_version(void)
+{
+	return (LIBHBSDCONTROL_VERSION);
+}
+
+hbsdctrl_ctx_t *
+hbsdctrl_ctx_new(hbsdctrl_flag_t flags)
+{
+	hbsdctrl_ctx_t *ctx;
+
+	if (!hbsdctrl_ctx_check_flag_sanity(flags)) {
+		return (NULL);
+	}
+
+	ctx = calloc(1, sizeof(*ctx));
+	if (ctx == NULL) {
+		return (NULL);
+	}
+
+	ctx->hc_version = LIBHBSDCONTROL_VERSION;
+	ctx->hc_flags = flags;
+
+	LIST_INIT(&(ctx->hc_features));
+
+	return (ctx);
+}
+
+void
+hbsdctrl_ctx_free(hbsdctrl_ctx_t **ctxp)
+{
+	hbsdctrl_feature_t *feature, *tfeature;
+	hbsdctrl_ctx_t *ctx;
+
+	if (ctxp == NULL || *ctxp == NULL) {
+		return;
+	}
+
+	ctx = *ctxp;
+	*ctxp = NULL;
+
+	LIST_FOREACH_SAFE(feature, &(ctx->hc_features), hf_entry, tfeature) {
+		LIST_REMOVE(feature, hf_entry);
+		hbsdctrl_feature_free(&feature);
+	}
+
+	free(ctx);
+}
+
+bool
+hbsdctrl_ctx_check_flag_sanity(hbsdctrl_flag_t flags)
+{
+	/* No flags supported, yet */
+	return (flags == 0);
+}
+
+hbsdctrl_flag_t
+hbsdctrl_ctx_get_flags(const hbsdctrl_ctx_t *ctx)
+{
+	if (ctx == NULL) {
+		return (0);
+	}
+
+	return (ctx->hc_flags);
+}
+
+hbsdctrl_flag_t
+hbsdctrl_ctx_set_flag(hbsdctrl_ctx_t *ctx, hbsdctrl_flag_t flag)
+{
+	hbsdctrl_flag_t oldflags;
+
+	if (ctx == NULL) {
+		return (0);
+	}
+
+	if (!hbsdctrl_ctx_check_flag_sanity(ctx->hc_flags | flag)) {
+		return (ctx->hc_flags);
+	}
+
+	oldflags = ctx->hc_flags;
+	ctx->hc_flags |= flag;
+	return (oldflags);
+}
+
+hbsdctrl_flag_t
+hbsdctrl_ctx_set_flags(hbsdctrl_ctx_t *ctx, hbsdctrl_flag_t flags)
+{
+	hbsdctrl_flag_t oldflags;
+
+	if (ctx == NULL) {
+		return (0);
+	}
+
+	if (!hbsdctrl_ctx_check_flag_sanity(flags)) {
+		return (ctx->hc_flags);
+	}
+
+	oldflags = ctx->hc_flags;
+	ctx->hc_flags = flags;
+	return (oldflags);
+}
+
+bool
+hbsdctrl_ctx_is_flag_set(const hbsdctrl_ctx_t *ctx, hbsdctrl_flag_t flag)
+{
+	if (ctx == NULL) {
+		return (false);
+	}
+
+	return ((ctx->hc_flags & flag) == flag);
+}
+
+hbsdctrl_feature_t *
+hbsdctrl_feature_new(hbsdctrl_ctx_t *ctx, const char *name, hbsdctrl_flag_t flags)
+{
+	hbsdctrl_feature_t *feature;
+
+	if (ctx == NULL || name == NULL) {
+		return (NULL);
+	}
+
+	feature = calloc(1, sizeof(*feature));
+	if (feature == NULL) {
+		return (NULL);
+	}
+
+	feature->hf_name = strdup(name);
+	if (feature->hf_name == NULL) {
+		free(feature);
+		return (NULL);
+	}
+
+	feature->hf_ctx = ctx;
+	feature->hf_flags = flags;
+
+	return (feature);
+}
+
+void
+hbsdctrl_feature_free(hbsdctrl_feature_t **featurep)
+{
+	hbsdctrl_feature_t *feature;
+
+	if (featurep == NULL || *featurep == NULL) {
+		return;
+	}
+
+	feature = *featurep;
+	*featurep = NULL;
+	free(feature->hf_name);
+	free(feature);
+}
+
+bool
+hbsdctrl_feature_flag_sanity(hbsdctrl_flag_t flag)
+{
+	/* No flags supported, yet */
+	return (flag == 0);
+}
+
+hbsdctrl_flag_t
+hbsdctrl_feature_get_flags(const hbsdctrl_feature_t *feature)
+{
+	if (feature == NULL) {
+		return (0);
+	}
+
+	return (feature->hf_flags);
+}
+
+hbsdctrl_flag_t
+hbsdctrl_feature_set_flag(hbsdctrl_feature_t *feature, hbsdctrl_flag_t flag)
+{
+	hbsdctrl_flag_t oldflags;
+
+	if (feature == NULL) {
+		return (0);
+	}
+
+	if (!hbsdctrl_feature_flag_sanity(feature->hf_flags | flag)) {
+		return (feature->hf_flags);
+	}
+
+	oldflags = feature->hf_flags;
+	feature->hf_flags |= flag;
+	return (oldflags);
+}
+
+hbsdctrl_flag_t
+hbsdctrl_feature_set_flags(hbsdctrl_feature_t *feature, hbsdctrl_flag_t flags)
+{
+	hbsdctrl_flag_t oldflags;
+
+	if (feature == NULL) {
+		return (0);
+	}
+
+	if (!hbsdctrl_feature_flag_sanity(flags)) {
+		return (feature->hf_flags);
+	}
+
+	oldflags = feature->hf_flags;
+	feature->hf_flags = flags;
+	return (oldflags);
+}
+
+bool
+hbsdctrl_feature_is_flag_set(const hbsdctrl_feature_t *feature, hbsdctrl_flag_t flag)
+{
+	if (feature == NULL) {
+		return (false);
+	}
+
+	return ((feature->hf_flags & flag) == flag);
+}
+
+const char *
+hbsdctrl_feature_get_name(const hbsdctrl_feature_t *feature)
+{
+	if (feature == NULL) {
+		return (NULL);
+	}
+
+	return ((const char *)(feature->hf_name));
+}
+
+void *
+hbsdctrl_feature_get_data(const hbsdctrl_feature_t *feature)
+{
+	if (feature == NULL) {
+		return (NULL);
+	}
+
+	return (feature->hf_data);
+}
+
+void *
+hbsdctrl_feature_set_data(hbsdctrl_feature_t *feature, void *data)
+{
+	void *olddata;
+
+	if (feature == NULL) {
+		return (NULL);
+	}
+
+	olddata = feature->hf_data;
+	feature->hf_data = data;
+	return (olddata);
+}
+
+void
+hbsdctrl_feature_set_init(hbsdctrl_feature_t *feature, hbsdctrl_feature_cb_t cb)
+{
+	if (feature == NULL) {
+		return;
+	}
+
+	feature->hf_init = cb;
+}
+
+void
+hbsdctrl_feature_set_cleanup(hbsdctrl_feature_t *feature, hbsdctrl_feature_cb_t cb)
+{
+	if (feature == NULL) {
+		return;
+	}
+
+	feature->hf_cleanup = cb;
+}
+
+void
+hbsdctrl_feature_set_pre_validate(hbsdctrl_feature_t *feature, hbsdctrl_feature_cb_t cb)
+{
+	if (feature == NULL) {
+		return;
+	}
+
+	feature->hf_pre_validate = cb;
+}
+
+void
+hbsdctrl_feature_set_validate(hbsdctrl_feature_t *feature, hbsdctrl_feature_cb_t cb)
+{
+	if (feature == NULL) {
+		return;
+	}
+
+	feature->hf_validate = cb;
+}
+
+void
+hbsdctrl_feature_set_apply(hbsdctrl_feature_t *feature, hbsdctrl_feature_cb_t cb)
+{
+	if (feature == NULL) {
+		return;
+	}
+
+	feature->hf_apply = cb;
+}
+
+void
+hbsdctrl_feature_set_unapply(hbsdctrl_feature_t *feature, hbsdctrl_feature_cb_t cb)
+{
+	if (feature == NULL) {
+		return;
+	}
+
+	feature->hf_unapply = cb;
+}
+
+hbsdctrl_feature_cb_res_t
+hbsdctrl_feature_call_cb(hbsdctrl_feature_t *feature, const char *name,
+    const void *arg1, void *arg2)
+{
+	if (feature == NULL || name == NULL) {
+		return (RES_FAIL);
+	}
+
+	if (!strcasecmp(name, "init") && feature->hf_init != NULL) {
+		return (feature->hf_init(feature->hf_ctx, feature, arg1,
+		    arg2));
+	}
+	if (!strcasecmp(name, "cleanup")) {
+		if (feature->hf_cleanup == NULL) {
+			return (RES_SUCCESS);
+		}
+		return (feature->hf_cleanup(feature->hf_ctx, feature, arg1,
+		    arg2));
+	}
+	if (!strcasecmp(name, "prevalidate")) {
+		if (feature->hf_pre_validate == NULL) {
+			return (RES_SUCCESS);
+		}
+		return (feature->hf_pre_validate(feature->hf_ctx, feature,
+		    arg1, arg2));
+	}
+	if (!strcasecmp(name, "validate")) {
+		if (feature->hf_validate == NULL) {
+			return (RES_SUCCESS);
+		}
+		return (feature->hf_validate(feature->hf_ctx, feature, arg1,
+		    arg2));
+	}
+	if (!strcasecmp(name, "apply") && feature->hf_apply != NULL) {
+		return (feature->hf_apply(feature->hf_ctx, feature, arg1,
+		    arg2));
+	}
+	if (!strcasecmp(name, "unapply") && feature->hf_unapply != NULL) {
+		return (feature->hf_unapply(feature->hf_ctx, feature, arg1,
+		    arg2));
+	}
+
+	return (RES_FAIL);
+}
