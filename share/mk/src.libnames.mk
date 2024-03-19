2aae872a58ef76787a02394455864330c0edcbaf
Shawn Webb <shawn.webb@hardenedbsd.org>
2024-03-19 00:17:59 +0000

    HBSD: Add more APIs and ABIs for libhbsdcontrol
    A given file's feature state might be one of:
    
    1. Enabled
    2. Disabled
    3. System default
    4. Unknown
    5. Invalid
    
    The vast majority of times, getting the state should return system
    default. A state is unknown when a system error occurs (perhaps
    filesystem extended attributes aren't available for the underlying
    filesystem.) A state of error occurs when an invalid combination of
    settings occurs. For example, attempting to set both the enabled and
    disabled filesystem extended attributes for a given file.
    
    This also starts the ASLR feature toggle. Provide the implementation for
    getting the feature state of a given file.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    (cherry picked from commit 485df84edab3b2419b161a2c1bddf655b47e911c)
    Signed-off-by: Shawn Webb <shawn.webb@hardenedbsd.org>

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -237,9 +237,8 @@ _LIBRARIES+= \
 .endif
 
 .if ${MK_HBSDCONTROL} != "no"
-_LIBRARIES+= \
-		hbsdcontrol \
-
+_LIBRARIES+= 		hbsdcontrol
+_DP_hbsdcontrol=	util
 .endif
 
 .if ${MK_OFED} != "no"
@@ -323,9 +322,6 @@ _DP_cap_grp=	nv
 _DP_cap_pwd=	nv
 _DP_cap_sysctl=	nv
 _DP_cap_syslog=	nv
-.if ${MK_HBSDCONTROL} != "no"
-_DP_hbsdcontrol=	sbuf
-.endif
 .if ${MK_OFED} != "no"
 _DP_pcap=	ibverbs mlx5
 .endif
