eae7a0fee80c959d853a92e96355f7a4ccd9ac2b
Shawn Webb <shawn.webb@hardenedbsd.org>
2024-03-19 20:57:02 +0000

    HBSD: Tie the ASLR feature in to the main context creation
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- lib/libhbsdcontrol/libhbsdcontrol.c
+++ lib/libhbsdcontrol/libhbsdcontrol.c
@@ -44,6 +44,7 @@ libhbsdctrl_get_version(void)
 hbsdctrl_ctx_t *
 hbsdctrl_ctx_new(hbsdctrl_flag_t flags, const char *ns)
 {
+	hbsdctrl_feature_t *feature;
 	hbsdctrl_ctx_t *ctx;
 
 	if (!hbsdctrl_ctx_check_flag_sanity(flags)) {
@@ -71,6 +72,16 @@ hbsdctrl_ctx_new(hbsdctrl_flag_t flags, const char *ns)
 	ctx->hc_flags = flags;
 
 	LIST_INIT(&(ctx->hc_features));
+
+	feature = hbsdctrl_feature_aslr_new(ctx, 0);
+	if (feature == NULL) {
+		hbsdctrl_ctx_free(&ctx);
+		return (NULL);
+	}
+	if (!hbsdctrl_ctx_add_feature(ctx, feature)) {
+		hbsdctrl_ctx_free(&ctx);
+		return (NULL);
+	}
 
 	return (ctx);
 }
