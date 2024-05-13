927fd28755da27c5dd2b1b0d0396c93db585f933
Shawn Webb <shawn.webb@hardenedbsd.org>
2024-05-13 16:35:57 +0000

    HBSD: Default _FORTIFY_SOURCE to 2
    Let's be good guinea pigs for enabling _FORTIFY_SOURCE across the base
    operating system. Further ports integration can be done.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Thanks-to:	e55512504d0178983978d64d67eed1cc85826523

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -294,13 +294,13 @@ CFLAGS.clang+=	 -Qunused-arguments
 # but not yet.
 CXXFLAGS.clang+=	 -Wno-c++11-extensions
 
-# XXX This should be defaulted to 2 when WITH_SSP is in use after further
-# testing and soak time.
-FORTIFY_SOURCE?=	0
 .if ${MK_SSP} != "no"
 # Don't use -Wstack-protector as it breaks world with -Werror.
 SSP_CFLAGS?=	-fstack-protector-strong
 CFLAGS+=	${SSP_CFLAGS}
+FORTIFY_SOURCE?=	2
+.else
+FORTIFY_SOURCE?=	0
 .endif # SSP
 .if ${FORTIFY_SOURCE} > 0
 CFLAGS+=	-D_FORTIFY_SOURCE=${FORTIFY_SOURCE}
