bb08a2d0297d340d8274e91b89ebdc6f294caead
Shawn Webb <shawn.webb@hardenedbsd.org>
2025-03-16 19:28:06 -0600

    HBSD: Resolve merge conflict
    Resolve this by bringing the CSU bits up-to-date with FreeBSD 14-STABLE.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- lib/csu/amd64/crt1_c.c
+++ lib/csu/amd64/crt1_c.c
@@ -27,47 +27,4 @@
  */
 
 #include <sys/cdefs.h>
-
-#include <stdlib.h>
-
-#include "libc_private.h"
-#include "ignore_init.c"
-
-#ifdef GCRT
-extern void _mcleanup(void);
-extern void monstartup(void *, void *);
-extern int eprol;
-extern int etext;
-#endif
-
-void _start(char **, void (*)(void));
-
-/* The entry function. */
-void
-_start(char **ap, void (*cleanup)(void))
-{
-	int argc;
-	char **argv;
-	char **env;
-
-	argc = *(long *)(void *)ap;
-	argv = ap + 1;
-	env = ap + 2 + argc;
-	handle_argv(argc, argv, env);
-
-	if (&_DYNAMIC != NULL) {
-		atexit(cleanup);
-	} else {
-		process_irelocs();
-		_init_tls();
-	}
-
-#ifdef GCRT
-	atexit(_mcleanup);
-	monstartup(&eprol, &etext);
-__asm__("eprol:");
-#endif
-
-	handle_static_init(argc, argv, env);
-	exit(main(argc, argv, env));
-}
+#include "csu_common.h"
--- lib/csu/common/csu_common.h
+++ lib/csu/common/csu_common.h
@@ -1,5 +1,6 @@
-/* LINTLIBRARY */
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
  * Copyright 1996-1998 John D. Polstra.
  * All rights reserved.
  *
@@ -24,7 +25,26 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
-#include "csu_common.h"
+#ifdef _CSU_COMMON_H_
 
-void _start(char *, ...) __dead2;
+/*
+ * This file includes both definitions and declarations, it can be
+ * included only into one compilation unit for csu objects.  We cannot
+ * practically check this, but at least guard against
+ * double-inclusion.
+ */
+#error "Include this file only once"
+#else
+#define _CSU_COMMON_H_
+
+char **environ;
+const char *__progname = "";
+
+#ifdef GCRT
+extern int eprol;
+extern int etext;
+#endif
+
+int main(int, char **, char **);
+
+#endif	/* _CSU_COMMON_H_ */
