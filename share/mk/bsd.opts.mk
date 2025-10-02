d7245925bfd134ce66e939be3039441da22d99e5
Shawn Webb <shawn.webb@hardenedbsd.org>
2025-10-02 10:28:44 -0600

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- share/mk/bsd.opts.mk
+++ share/mk/bsd.opts.mk
@@ -76,11 +76,7 @@ __DEFAULT_NO_OPTIONS = \
     CTF \
     INSTALL_AS_USER \
     PROFILE \
-<<<<<<< HEAD
-=======
     REPRODUCIBLE_BUILD \
-    RETPOLINE \
->>>>>>> origin/freebsd/14-stable/main
     STALE_STAGED \
     UBSAN
 
